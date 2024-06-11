ed5a3fabdfab246defee61ef22afa39938aace90
elderbranch <151013203+elderbranch@users.noreply.github.com>
2024-06-11 17:37:31 +0600

    OK

--- invest-chat/src/pages/Modals/SignInModal.jsx
+++ invest-chat/src/pages/Modals/SignInModal.jsx
@@ -1,15 +1,15 @@
 import "./SignInModal.scss"
 import { ButtonFactory } from "../../components/UI/Buttons/Buttons";
 
-const SignInModal = ({active, setActive}) => {
+const SignInModal = ({ active, setActive }) => {
   return (
     <main className={active ? "Sign_active" : "Sign"} onClick={() => setActive(false)} >
       <div className={active ? "Sign_block" : "block"} onClick={e => e.stopPropagation()}>
         <form action="" className="Sign_form">
           <label className="Sign_title">Вход на сайт</label>
           <span className="Sign_text">Войди и получи ответы на свои вопросы</span>
-          <input className="Sign_number_input" type="phone" name="phone-number" placeholder="Номер телефона"/>
-          <input className="Sign_email_input" type="email" name="email-address" placeholder="Email"/>
+          <input className="Sign_number_input" type="phone" name="phone-number" placeholder="Номер телефона" />
+          <input className="Sign_email_input" type="email" name="email-address" placeholder="Email" />
           <div className="Sign_or">
             или
           </div>
