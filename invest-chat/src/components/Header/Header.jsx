84bb7af97ca7db5a7f66d79acdabb58eba5a2b81
dispatcherenko <chda.contact@gmail.com>
2024-06-14 14:08:39 +0700

    Minor

--- invest-chat/src/components/Header/Header.jsx
+++ invest-chat/src/components/Header/Header.jsx
@@ -1,8 +1,7 @@
 import "./Header.scss";
 import { useNavigate } from "react-router-dom";
 import ButtonFactory from "../UI/Buttons/Buttons";
-import Door from "../../assets/door.svg";
-import Logo from "../../assets/logo.svg";
+import { ReactSVG } from "react-svg";
 
 const Header = ({ setModalActive }) => {
   const navigate = useNavigate();
@@ -15,28 +14,44 @@ const Header = ({ setModalActive }) => {
         }}
         className="header__logo"
       >
-        <img src={Logo} alt="logo" />
+        <ReactSVG src="assets/logo.svg" />
       </div>
       <nav className="header__nav">
         <ButtonFactory type="white" onClick={() => setModalActive(true)}>
           Войти
         </ButtonFactory>
-        <ButtonFactory
-          type="white"
+        <ReactSVG
+          className="header__link"
+          src="../../assets/bell.svg"
+          alt="user"
           onClick={() => {
             navigate("/profile");
           }}
-        >
-          Профиль
-        </ButtonFactory>
+        />
+        <ReactSVG
+          className="header__link"
+          src="../../assets/user.svg"
+          alt="user"
+          onClick={() => {
+            navigate("/profile");
+          }}
+        />
         <ButtonFactory
           onClick={() => {
             navigate("/registration");
           }}
           type="white"
         >
-          <img src={Door} alt="door" />
+          <img src="/assets/door.svg" alt="door" />
           Регистрация
+        </ButtonFactory>
+        <ButtonFactory
+          onClick={() => {
+            navigate("/");
+          }}
+          type="red"
+        >
+          Спроси у помощника
         </ButtonFactory>
       </nav>
     </header>
